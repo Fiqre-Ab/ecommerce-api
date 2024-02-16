0901ee66136ed540778eaa3c9986aa46283ce1d0
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2024-02-15 23:27:13 -0800

    router

--- models/User.js
+++ models/User.js
@@ -1,28 +1,27 @@
 const mongoose = require("mongoose");
-const { boolean } = require("wbidl-converstions")
+
 const UserSchema = new mongoose.Schema(
     {
         username: {
             type: String,
             required: true,
             unique: true
-
         },
         email: {
             type: String,
             required: true,
             unique: true
-
         },
         password: {
             type: String,
             required: true
         },
         isAdmin: {
-            type: boolean,
+            type: Boolean, 
             default: false
         }
     },
     { timestamps: true }
-)
-module.exports = mongoose.model(user, UserSchema);
+);
+
+module.exports = mongoose.model("User", UserSchema); 
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,23 @@
 const router = require("express").Router();
+const User = require("../models/User")
 
-router.get("/usertest", (req,res) => {
-    res.send("User is successful");
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+
+    })
+    try {
+        const newsave = await newUser.save()
+            .then((newsave) => {
+                res.status(201).json(newsave); // Send the saved user object back to the client
+            })
+    } catch (err) {
+        // Handle any errors that occur during the save process
+        res.status(500).json(err);
+    }
 
 })
-router.post("/postest",(req,res)=>{
-    const username=req.body.username;
-    res.send("the username is "+username);
-})
 
-module.exports =router;
+module.exports = router;
